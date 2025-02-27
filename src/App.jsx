8279a903bb6a7bce8d65a65560b0f7777ae75630
osama0210 <osama.asmi04@gmail.com>
2025-02-27 11:52:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import header from "/components/header.jsx"
+
+const App = () => {
+    return (
+        <>
+        <header>
+
+        </header>
+        </>
+    )
+}
